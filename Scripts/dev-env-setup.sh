91445bc07640292d385f03c56a386b68435dfb6c
Byung Kang <bkang61wk@gmail.com>
2017-01-23 18:20:07 -0500

    Updated script to install and configure mongoDB

--- Scripts/dev-env-setup.sh
+++ Scripts/dev-env-setup.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+# update packages info
+sudo apt update
+
+# upgrade installed packages
+sudo apt -y upgrade
+
 # install apache2
-sudo apt install apache2
+sudo apt -y install apache2
 
 # install php
-sudo apt install php libapache2-mod-php php-mcrypt
+sudo apt -y install php libapache2-mod-php php-mcrypt
 
 # install composer (for slim framework)
-sudo apt install composer
+sudo apt -y install composer
 
 # install slim framework
 cd ..
 #composer require slim/slim "^3.0"  # This is for initial install. No need to run this more than once since repo contains composer.lock
 composer install
+cd Scripts
+
+# install mongoDB
+# https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-16-04
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927  # import key
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list  # create a list file for MongoDB
+sudo apt update
+sudo apt install -y mongodb-org
+
+sudo cp mongodb.service /etc/systemd/system/mongodb.service  # add mongoDB to systemd
+
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+#sudo systemctl status mongodb
+
+# to remove remove startup warnings (optional)
+# https://docs.mongodb.com/manual/tutorial/transparent-huge-pages/#transparent-huge-pages-thp-settings
+sudo cp disable-transparent-hugepages /etc/init.d/disable-transparent-hugepages
+sudo chmod 755 /etc/init.d/disable-transparent-hugepages
+sudo update-rc.d disable-transparent-hugepages defaults
