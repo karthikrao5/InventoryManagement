e24d796792d1c31236f2282926b7d9bcde6fd5ad
Byung Kang <bkang61wk@gmail.com>
2017-02-03 18:19:45 -0500

    Fixed move.sh to move all contents of public/ to /var/www/html and vendor/ to /var/www

--- move.sh
+++ move.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo cp -rp public/core/ /var/www/html
-#sudo cp -p public/index.php /var/www/html
-#sudo cp -p public/.htaccess /var/www/html
-sudo cp -p public/* /var/www/html
-sudo cp -rp vendor /var/www
+sudo cp -a public/. /var/www/html
+sudo cp -a vendor /var/www
 
