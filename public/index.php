3127f4bc37239fb6e92213f533b5bfc32b61ecd4
krao34 <krao34@gatech.edu>
2017-02-17 18:00:46 -0500

    saving progress

--- public/index.php
+++ public/index.php
@@ -2,11 +2,39 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-session_start();
+use Doctrine\MongoDB\Connection;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+
 
 # import necessary packages from composer
-require __DIR__ . '/../vendor/autoload.php';
+if (! file_exists($file= __DIR__.'/../vendor/autoload.php')) {
+	throw new RuntimeException('Install dependencies to run this script.');
+}
 
+$loader = require_once $file
+$loader->add('/../src/Models', __DIR__);
+
+$connection = new Connection();
+$config = new Configuration();
+$config->setProxyDir(__DIR__.'/../src/Proxies');
+$config->setProxyNamespace('Proxies');
+$config->setHydratorDir(__DIR__.'/../src/Hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setDefaultDB('doctrine_odm')
+;
+$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__.'/../src/Models'));
+
+AnnotationDriver::registerAnnotationClasses();
+$dm = DocumentManager::create($connection, $config);
+
+// use above for autoloader for ODM since you use $loader for creating documents
+// require __DIR__ . '/../vendor/autoload.php';
+
+
+session_start();
 
 // get settings from settings file
 $settings = require __DIR__ . '/../src/settings.php';
