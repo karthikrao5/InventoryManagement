d442d762866fe1cba0c523089811f069c1d7788a
krao34 <krao34@gatech.edu>
2017-03-23 21:46:49 -0400

    added log model file (no functionality)

---
+++ src/Models/Log.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ODM\Document(db="inventorytracking")
+ */
+class Log {
+
+
+}
