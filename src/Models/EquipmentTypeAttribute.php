5d884e82fa54c700203cbfed4764dbb6c3d4d2f5
krao34 <krao34@gatech.edu>
2017-03-27 11:15:47 -0400

    moved EquipmentTypeAttribute to its own file

--- src/Models/EquipmentTypeAttribute.php
+++ src/Models/EquipmentTypeAttribute.php
@@ -3,37 +3,63 @@
 namespace App\Models;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Doctrine\Common\Collections\ArrayCollection;
 
-/** @ODM\EmbeddedDocument */
-class EquipmentTypeAttribute {
-
+/**
+ * @ODM\EmbeddedDocument
+ */
+class EquipmentTypeAttribute
+{
 	/** @ODM\Field(type="string") */
-	public $name;
-	public function getName() { return $this->name; }
-	public function setName($newName) { $this->name = $newName; }
-
-	/** ODM\Bool */
-	public $required;
-	public function isRequired() { return $this->required; }
-	public function setRequired($bool) { $this->required = $bool; }
-
-	/** @ODM\Bool */
-	public $unique;
-	public function isUnique() { return $this->unique; }
-	public function setUnique($bool) { $this->unique = $bool; }
-
+	private $name;
+	
+	/** @ODM\Field(type="boolean") */
+	private $required;
+	
+	/** @ODM\Field(type="boolean") */
+	private $unique;
+	
 	/** @ODM\Field(type="string") */
-	public $data_type;
-	public function getDataType() { return $this->data_type; }
-	public function setDataType($string) { $this->data_type = $string; }
-
-	/** @ODM\Field(type="string") */
-	public $regex;
-	public function getRegex() { return $this->regex; }
-	public function setRegex($string) { $this->regex = $regex; }
-
-	/** @ODM\Field(type="string") */
-	public $help_comment;
-	public function getHelpComment() { return $this->help_comment; }
-	public function setHelpComment($string) { $this->help_comment = $string; }	
+	private $data_type;
+	
+	/** @ODM\Field(type="string", nullable=true) */
+	private $regex;
+	
+	/** @ODM\Field(type="string", nullable=true) */
+	private $help_comment;
+	
+	/** @ODM\Field(type="boolean") */
+	private $enum;
+	
+	/** @ODM\Field(type="collection", nullable=true) */
+	private $enum_values;
+	
+	public function __construct()
+	{
+		$this->enum_values = new ArrayCollection();
+	}
+	
+	public function setName($name) {$this->name = $name;}
+	public function getName() {return $this->name;}
+	
+	public function setRequired($required) {$this->required = $required;}
+	public function getRequired() {return $this->required;}
+	
+	public function setUnique($unique) {$this->unique = $unique;}
+	public function getUnique() {return $this->unique;}
+	
+	public function setDataType($dataType) {$this->data_type = $dataType;}
+	public function getDataType() {return $this->data_type;}
+	
+	public function setRegex($regex) {$this->regex = $regex;}
+	public function getRegex() {return $this->regex;}
+	
+	public function setHelpComment($helpComment) {$this->help_comment = $helpComment;}
+	public function getHelpComment() {return $this->help_comment;}
+	
+	public function setEnum($enum) {$this->enum = $enum;}
+	public function getEnum() {return $this->enum;}
+	
+	public function setEnumValues($enum_values) {$this->enum_values = $enum_values;}
+	public function getEnumValues() {return $this->enum_values;}
 }
