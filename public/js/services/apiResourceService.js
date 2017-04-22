27f325c06e85d6cbb99bdf4945d8c0af08c776cf
krao34 <krao34@gatech.edu>
2017-04-22 16:27:55 -0400

    Edit equipment frontend works now

--- public/js/services/apiResourceService.js
+++ public/js/services/apiResourceService.js
@@ -1,48 +1,31 @@
-angular.module("app").factory("APIService", ["$scope", "$window", "$http",
-	function($window, $http, $scope) {
+angular.module("app").factory("APIService", ["$http", "$httpParamSerializerJQLike",
+	function($http,$httpParamSerializerJQLike, $routeParams) {
 		var apiURL = "v1/";
 
-		// var config = {
-		// 	paramSerializer: '$httpParamSerializerJQLike'
-		// };
-
-		$scope.get = function(resource, params, success, error) {
-			$http.get(apiURL+resource, params).then(success).catch(error)
-		}
-
-		$scope.post = function(resource, data, success, error) {
-			console.log(apiURL+resource);
-			$http.post(apiURL+resource, data).then(success).catch(error)
-		}
-
-		$scope.query = function(resource, success, error) {
-			// console.log("here");
-			$http.get(apiURL + resource).then(success).catch(error)
-		}
-
-		$scope.delete = function(resource, params, success, error) {
-			$http.delete(apiURL+resource, params).then(success).catch(error)
-		}
-
-		$scope.put = function(resource, data, params, success, error) {
-			$http.put(apiURL+resource).then(success).catch(error)
-		}
-
 		return {
-			get : function(resource, params, success, error) {
-				return this.get(resource, params, success, error);
+
+			// search with params
+			get : function(resource, inputParams, success, error) {
+				$http.get(apiURL+resource, {params: $httpParamSerializerJQLike(inputParams)}).then(success).catch(error)
 			},
+
 			post : function(resource, data, success, error) {
-				return this.post(resource, data, success, error);
+				console.log(apiURL+resource);
+				$http.post(apiURL+resource, data).then(success).catch(error)
 			},
+
+			// get all items
 			query : function(resource, success, error) {
-				return this.get(resource, success, error);
+				// console.log("here");
+				$http.get(apiURL + resource).then(success).catch(error)
 			},
+
 			delete : function(resource, params, success, error) {
-				return this.delete(resource, params, success, error);
+				$http.delete(apiURL+resource, params).then(success).catch(error)
 			},
+
 			put : function(resource, data, params, success, error) {
-				return this.put(resource, data, params, success, error);
+				$http.put(apiURL+resource).then(success).catch(error)
 			}
 		};
 	}
