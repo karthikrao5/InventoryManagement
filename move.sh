063ccfbdfe9dc72077c0448a646a73451d59d3dc
Byung Kang <bkang61wk@gmail.com>
2017-04-22 12:32:35 -0400

    Removed ODM related stuff 2.

--- move.sh
+++ move.sh
@@ -3,5 +3,4 @@ sudo cp -a public/. /var/www/html
 sudo cp -a vendor /var/www
 sudo cp -a src /var/www
 
-sudo chmod -R 777 /var/www/src/Helper/odmcache/
 sudo chmod -R 777 /var/www/src/log/
--- src/routes.php
+++ src/routes.php
@@ -16,66 +16,6 @@ $app->get('/', function($request, $response) {
     $this->view->render($response, 'index.html');
 });
 
-$app->group('/frontend', function() {
-
-    // $this->get('/', 'ViewController:index')->setName("root");
-
-    // $this->get('/add-equipment', 'ViewController:getEquipmentForm')->setName("get-add-equipment");
-    // $this->post("/add-equipment", 'ViewController:postEquipmentForm');
-
-    // $this->get('/add-equipment-type', 'ViewController:getEquipmentTypeForm')->setName("get-add-equipment-type");
-    // $this->post('/add-equipment-type', 'ViewController:postEquipmentTypeForm');
-
-    // $this->get("/show-all-equipmenttypes", "ViewController:showAllEquipmentTypes")->setName("all-equipment-types");
-
-    // $this->get("/edit-equipment[/{params:.*}]", "ViewController:editEquipment")->setName("edit-equipment");
-    // $this->put("/edit-equipment", "ViewController:updateEquipment");
-
-
-    // $this->delete('/delete-equipment[/{params:.*}]', 'ViewController:deleteEquipment')->setName("delete-equipment");
-
-});
-
-
-
-$app->get("/test", function($request, $response) {
-    return $this->view->render($response, "hp.html", array());
-});
-
-// $app->get('/home', function($request, $response) {
-//     // $this->logger->info("reached /home");
-//     return $this->view->render($response, 'template.html');
-// });
-
-// $app->get('/equipment', function($request, $response) {
-//     // $this->logger->info("reached /home");
-//     return $this->view->render($response, 'hp.html', array(data => getAll()));
-// });
-
-
-// $app->get('/equipment/{id}', function($request, $response) {
-//     // $this->logger->info("reached /home");
-//     $id = $request->getAttribute('id');
-//     $core = CoreService::getInstance();
-//     $result = $core->getEquipmentById($request->getAttribute('id'));
-//     // $json_response = $response->withJson($result);
-//     $data =  $result['equipment'];
-
-$app->get('/home', function($request, $response) {
-    return $app->get("view")->render($response, "hp.html");
-});
-//     return $this->view->render($response, 'equipmentpage.html', array(data => $data));
-// });
-$app->get('/addequipment', function($request, $response) {
-    // $this->logger->info("reached /home");
-    return $this->view->render($response, 'addequipment.html');
-});
-$app->get('/addequipmenttype', function($request, $response) {
-    // $this->logger->info("reached /home");
-    return $this->view->render($response, 'addequipmenttype.html');
-});
-
-
 // Backend routes.
 $app->group('/v1', function() {
 
@@ -105,29 +45,29 @@ $app->group('/v1', function() {
 
         $this->put('', 'EquipmentTypeController:updateOne');
     });
-    
+
     // log routes
     $this->group('/logs', function() {
         $this->get('[/{params:.*}]', 'LogController:find');
     });
-    
+
     // user routes
     $this->group('/users', function() {
         $this->post('', 'UserController:create');
-        
+
         $this->get('[/{params:.*}]', 'UserController:find');
-        
+
         $this->delete('', 'UserController:delete');
 
         $this->put('', 'UserController:update');
     });
-    
+
     // loan routes
     $this->group('/loans', function() {
         $this->post('', 'LoanController:create');
-        
+
         $this->get('[/{params:.*}]', 'LoanController:get');
-        
+
         $this->delete('', 'LoanController:delete');
 
         $this->put('', 'LoanController:update');
