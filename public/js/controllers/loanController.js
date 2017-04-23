e787bdd2a159b53d39755e31df5e836eb31e1846
Joe Sadler <joesadler71@gmail.com>
2017-04-23 01:13:44 -0400

    added create loan post

--- public/js/controllers/loanController.js
+++ public/js/controllers/loanController.js
@@ -1,22 +1,27 @@
-angular.module("app.controllers").controller("LoanController", ["$scope", "$http", "$location", "$routeParams", "$window", "APIService", "uiGridConstants","$filter",
+angular.module("app.controllers").controller("LoanController", ["$scope", "$http", "$location", "$routeParams", "$window", "APIService", "uiGridConstants","$filter", 
 	function($scope, $http, $location, $routeParams, $window, APIService) {
 
 		// $scope.originalItem;
-
+		$scope.equipmentList = {};
+		$scope.equipmentList["equipments"] = [];
 		console.log($routeParams.username);
 		$scope.paramsArray = {};
-		$scope.paramsArray["username"] = $routeParams.username;
+		$scope.equipmentList["username"] = $routeParams.username;
 		console.log($scope.paramsArray);
 
-		function onSuccess(response) {
-	    	var local = response.data.equipments;
-	    	$scope.gridOptions.data = response.data.equipments;
-	    }
+		// $scope.gridOptions.onRegisterApi = function(gridApi) {
+		//    $scope.myGridApi = gridApi;
+		//    };
 
 	   // make database call
 	    APIService.query("equipments", onSuccess, function(error) {
 	    	console.log(error);
 	    });
+
+	    function onSuccess(response) {
+	    	var local = response.data.equipments;
+	    	$scope.gridOptions.data = response.data.equipments;
+	    }
 
 		$scope.refreshData = function() {
 			$scope.gridOptions.data = $filter('filter')($scope.data, $scope.searchText);
@@ -31,14 +36,49 @@ angular.module("app.controllers").controller("LoanController", ["$scope", "$http
 	    				  {field: "created_on", enableHiding: false}
 	    			];
 
-
+	   
 	    $scope.gridOptions = {
 	    	enableSorting: true,
 	    	columnDefs: $scope.columns,
 	    	enableGridMenu: true,
 	    	multiSelect: true
+	    // 	onRegisterApi: function( gridApi ) {
+    	// 		$scope.gridApi = gridApi;
+  			// };
 
 	    };
+	    $scope.gridOptions.onRegisterApi = function( gridApi ) {
+    		$scope.gridApi = gridApi;
+  		};
+
+	    $scope.submitLoan = function() {
+	    	$scope.items = $scope.gridApi.selection.getSelectedRows();
+	    	angular.forEach($scope.items, function(value, key) {
+			  this.push(value.department_tag);
+			}, $scope.equipmentList["equipments"]);
+			// console.log($scope.equipmentList["equipments"]);
+
+			$scope.equipmentList["due_date"] = "2017-05-01 23:59:59";
+
+			console.log(angular.toJson($scope.equipmentList));
+
+			function onSuccess(response) {
+				alert("Successfully created Loan!");
+				console.log(response.data);
+			}
+
+			APIService.post("loans", $scope.equipmentList, onSuccess, function(error) {
+				alert(error);
+			});
+
+
+			$scope.equipmentList["equipments"] = [];
+
+
+
+
+		};
+
 
 	    $scope.toggleFiltering = function() {
 	    	console.log("Filtering tbd...");
